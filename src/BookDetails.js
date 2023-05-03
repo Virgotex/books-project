0fa6a8554ae1d4617f580bd0629d41688cafcfb4
Cassie <cassierasugu@gmail.com>
2023-05-03 14:49:04 +0300

    add Layout.js

---
+++ src/BookDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function BookDetails({ book }) {
+    return (
+      <div>
+        <h1>Please select a book</h1>
+      </div>
+  );
+  }
+  export default BookDetails;
